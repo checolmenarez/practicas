9e1cd31dc97aba2b0b1828e900d3940b8816f255
Jose Colmenarez <checolmenarez23@gmail.com>
2017-08-17 17:29:33 -0400

    Avance

--- Practicas/src/com/practicas/basico/Matrices.java
+++ Practicas/src/com/practicas/basico/Matrices.java
@@ -4,24 +4,42 @@ import java.io.*;
 import java.util.*;
 
 public class Matrices {
-
-    public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-    	int[] arr;
+	
+    public int DiferenciaDiagonal(int n, ArrayList lista) {
+    	int dif=0;
+    	try {
+			
+		} catch (Exception e) {
+			
+		}
+    	return dif;
+    			
+    }
+    public static <E> void main(String[] args) {
+        ArrayList<int[]> lista = new ArrayList<int[]>();
+    	int[] arr=null;
         Scanner in = new Scanner(System.in);
-        System.out.println("Ingrese le orden de la matriz");
+        
+        System.out.println("Ingrese el orden de la matriz");
         int numfilas = in.nextInt();
         
         for(int y=0;y<numfilas;y++) {
         	
-        	arr = new int[numfilas];        
-            System.out.println("Ingrese los elementos de la fila Nro :"+y);
+        	System.out.println("Ingrese los elementos de la fila Nro :"+y);
+        	arr = new int[3];
             for(int i=0;i<numfilas;i++) {
             	arr[i] = in.nextInt();
-            }
-        	
+            }        	
+            lista.add(y,arr);
         }
         
+        System.out.println("Matriz: \n");
+        
+        for(int x=0;x<numfilas;x++) {
+        	for(int y=0;y<numfilas;y++) {
+        		System.out.println(lista.get(x)[y]);
+        	}
+        }
         
     }
 }
